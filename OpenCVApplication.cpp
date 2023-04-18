0a6f881aa6448422bea5ec18fd98bcf81c07e2ce
Ardelean Raluca <ardelean.raluca.3@gmail.com>
2023-04-18 18:55:34 +0300

    RANSAC with linii prelungire

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -13,22 +13,40 @@
 using namespace std;
 using namespace cv;
 
+
+double getLength(Point a, Point b) {
+    double sum = sqrt(pow((b.x - a.x), 2) + pow((a.y - b.y), 2));
+    return sum;
+}
+
+
+bool inside(Mat img, int x, int y) {
+    if (x >= 0 && x < img.rows && y >= 0 && y < img.cols)
+        return true;
+    return false;
+}
+
+
 void FitLineRANSAC(
+        Mat canny,
         vector<Point> points_,
         double threshold_,
         int maximum_iteration_number_,
-        Mat image_) {
+        Mat image_, void *) {
     srand(time(NULL));
     int iterationNumber = 0;
 
     vector<int> inliers;
+    vector<pair<Point, Point>> lines;
     inliers.reserve(points_.size());
 
     constexpr int kSampleSize = 2;
     std::vector<int> sample(kSampleSize);
     bool shouldDraw = image_.data != nullptr;
     while (iterationNumber++ < maximum_iteration_number_) {
+        int found = 0;
         // 1. Select a minimal sample, i.e., in this case, 2 random points.
+
         for (size_t sampleIdx = 0; sampleIdx < kSampleSize; ++sampleIdx) {
             do {
                 sample[sampleIdx] =
@@ -36,10 +54,13 @@ void FitLineRANSAC(
                 if (sampleIdx == 0)
                     break;
                 if (sampleIdx == 1 &&
-                    sample[0] != sample[1])
+                    sample[0] != sample[1] &&
+                    getLength(points_[sample[0]], points_[sample[1]]) > 150)
                     break;
             } while (true);
         }
+
+
         // 2. Fit a line to the points.
         const Point2d &p1 = points_[sample[0]]; // First point selected
         const Point2d &p2 = points_[sample[1]]; // Second point select
@@ -67,52 +88,215 @@ void FitLineRANSAC(
             }
         }
 
-        if (inliers.size() > 150 && shouldDraw) {
+
+        if (inliers.size() > 100 && shouldDraw) {
+            lines.push_back(make_pair(p1, p2));
             for (size_t pointIdx = inliers.size() - 1; pointIdx > 0; --pointIdx) {
                 points_.erase(points_.begin() + inliers[pointIdx]);
             }
             inliers.clear();
             inliers.resize(0);
-            cv::line(image_,
-                     p1,
-                     p2,
-                     cv::Scalar(0, 0, 255),
-                     1);
+
+
+//            cv::line(image_,
+//                     p1,
+//                     p2,
+//                     cv::Scalar(255, 0, 0),
+//                     3);
+        }
+    }
+
+    for (int i = 0; i < lines.size(); i++) {
+        float slope =  (lines[i].first.y - lines[i].second.y) / (1.0 * (lines[i].first.x - lines[i].second.x));
+        Point2d v = lines[i].second - lines[i].first; // Direction of the line
+        v = v / cv::norm(v); // Division by the length of the vector to make it unit length
+        Point2d n; // Normal of the line (perpendicular to the line)
+        // Rotate v by 90° to get n
+        n.x = -v.y;
+        n.y = v.x;
+        // distance(line, point) = | a * x + b * y + c | / sqrt(a * a + b * b)
+        // if ||n||_2 = 1 (unit length) then sqrt(a * a + b * b) = 1 and I don't have to do the division that is in the previous line
+        long double a = n.x;
+        long double b = n.y;
+        long double c = -(a * lines[i].first.x + b * lines[i].first.y);
+
+        int xmax = max(lines[i].first.x, lines[i].second.x);
+        int xmin = min(lines[i].first.x, lines[i].second.x);
+        int ymin = min(lines[i].first.y, lines[i].second.y);
+        int ymax = max(lines[i].first.y, lines[i].second.y);
+
+        Point minPoint = lines[i].first;
+        Point maxPoint = lines[i].second;
+
+        int dx[] = {0, 0, -1, 1, 0, 0, 0, -2, 2, 0, 0, -3, 3};
+        int dy[] = {1, -1, 0, 0, 0, 2, -2, 0, 0, 3, -3, 0, 0};
+
+
+        if (lines[i].first.x - lines[i].second.x == 0) {
+            while (true) {
+
+                ymax++;
+                Point newPoint = Point(lines[i].first.x, ymax);
+                int ok = 0;
+                for (int k = 0; k < 9; k++) {
+                    int y = newPoint.x + dx[k];
+                    int x = newPoint.y + dy[k];
+                    if (!inside(image_, x,y)) {
+                        break;
+                    } else {
+                        if (canny.at<uchar>(x, y) > 128) {
+                            ok = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (ok == 0)
+                    break;
+
+
+                maxPoint = newPoint;
+            }
+
+            while (true) {
+                ymin--;
+                int ok = 0;
+                Point newPoint = Point(lines[i].first.x, ymin);
+                for (int k = 0; k < 9; k++) {
+                    int y = newPoint.x + dx[k];
+                    int x = newPoint.y + dy[k];
+                    if (!inside(image_, x,y)) {
+                        break;
+                    } else {
+                        if (canny.at<uchar>(x, y) > 128) {
+                            ok = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (ok == 0)
+                    break;
+                minPoint = newPoint;
+            }
+
+        } else {
+            while (true) {
+                xmax++;
+                Point newPoint = Point(xmax, slope * xmax + c);
+                int ok = 0;
+                for (int k = 0; k < 9; k++) {
+                    int y = newPoint.x + dx[k];
+                    int x = newPoint.y + dy[k];
+                    if (!inside(image_, x,y)) {
+                        break;
+                    } else {
+                        if (canny.at<uchar>(x, y) > 128) {
+                            ok = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (ok == 0)
+                    break;
+                maxPoint = newPoint;
+            }
+
+            while (true) {
+                xmin--;
+                Point newPoint = Point(xmin, slope * xmin + c);
+                int ok = 0;
+                for (int k = 0; k < 9; k++) {
+                    int y = newPoint.x + dx[k];
+                    int x = newPoint.y + dy[k];
+                    if (!inside(image_, x,y)) {
+                        break;
+                    } else {
+                        if (canny.at<uchar>(x, y) > 128) {
+                            ok = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (ok == 0)
+                    break;
+                minPoint = newPoint;
+            }
+        }
+        cv::line(image_,
+                 minPoint,
+                 maxPoint,
+                 cv::Scalar(0, 0, 255),
+                 2);
+    }
+
+}
+
+vector<Point> points;
+Mat input;
+int lowThreshold = 0;
+int maxThreshold = 100;
+int r = 3;
+
+//static void on_track(int, void *) {
+//    FitLineRANSAC(points, lowThreshold * r, 100000, input, 0);
+//}
+
+void DrawPoints(vector<Point> &points, Mat image) {
+    for (int i = 0; i < points.size(); ++i) {
+        circle(image, points[i], 1, Scalar(0, 255, 0));
+    }
+}
+
+void f(Mat img, vector<Point> &points) {
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {
+            uchar pix = img.at<uchar>(i, j);
+            if (pix > 127) {
+                points.push_back(Point(j, i));
+            }
         }
     }
 }
 
-void DrawPoints(vector<Point> &points, Mat image) {
-    for (int i = 0; i < points.size(); ++i) {
-        circle(image, points[i], 1, Scalar(255, 255, 255));
-    }
-}
+
+const std::string window = "test";
 
 int main() {
-    Mat input = imread("ransac/canny9.jpg", 1);
+    input = imread("ransac/canny9.jpg", 1);
 
-//    if (input.data == nullptr) {
-//        cerr << "Failed to load image" << endl;
-//    }
+    if (input.data == nullptr) {
+        cerr << "Failed to load image" << endl;
+    }
     Mat gray;
     Mat edge_gaus;
 
     int kernel_size = 5;
     Mat blur_gray;
     cvtColor(input, gray, COLOR_BGR2GRAY);
-    GaussianBlur(gray, blur_gray, Size(5, 5), 1.4);
-    Canny(blur_gray, edge_gaus, 100, 120, 3, true);
+    GaussianBlur(gray, blur_gray, Size(5, 5), 1.4, 1.4);
+    Canny(blur_gray, edge_gaus, 100, 120, 3);
     //imshow("Gaus", edge_gaus);
+//
+//    Mat image = Mat::zeros(edge_gaus.rows, edge_gaus.cols, CV_8UC3);
 
-    Mat image = Mat::zeros(edge_gaus.rows, edge_gaus.cols, CV_8UC3);
-    vector<Point> points;
 
     findNonZero(edge_gaus, points);
-    //DrawPoints(points, input);
-    FitLineRANSAC(points, 3, 100000, input);
+//    DrawPoints(points, input);
 
+//    cv::namedWindow(window, cv::WINDOW_AUTOSIZE);
+
+//    cv::createTrackbar("Threshold: ", window, &lowThreshold, maxThreshold, on_track);
+
+//    on_track(0,0);
+    FitLineRANSAC(edge_gaus,points, 1, 100000, input, 0);
+
+//
     imshow("Edge detected image", edge_gaus);
     imshow("Output", input);
+    moveWindow("Output", 300, 200);
     waitKey(0);
 
     return 0;

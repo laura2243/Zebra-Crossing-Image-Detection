9b71e24606ac68c77df78cdeb2f08f4cc12a8bcc
Ardelean Raluca <ardelean.raluca.3@gmail.com>
2023-05-03 20:31:41 +0300

    -- affine transform

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -47,25 +47,81 @@ Mat Canny(Mat src) {
 
 int main() {
     Mat input;
+    Mat idealPhoto;
     input = imread("ransac/canny6.jpg", 1);
+    idealPhoto = imread("ransac/cannyR.jpg", 1);
 
     if (input.data == nullptr) {
         cerr << "Failed to load image" << endl;
     }
     Mat gray;
+    Mat grayIdeal;
 
     cvtColor(input, gray, COLOR_BGR2GRAY);
+    cvtColor(idealPhoto, grayIdeal, COLOR_BGR2GRAY);
 
     Mat canny = Canny(gray);
+    Mat cannyIdeal = Canny(grayIdeal);
 
     vector<Vec4i> linesP; // will hold the results of the detection
-    HoughLinesP(canny, linesP, 0.5, CV_PI / 180, 40, 20, 20); // runs the actual detection
+    vector<Vec4i> linesIdeal;
+    HoughLinesP(canny, linesP, 0.5, CV_PI / 180, 40, 50, 20); // runs the actual detection
+    HoughLinesP(cannyIdeal, linesIdeal, 0.5, CV_PI / 180, 40, 50, 20);
     // Draw the lines
-    for (size_t i = 0; i < linesP.size(); i++)
-    {
+    for (size_t i = 0; i < linesP.size(); i++) {
         Vec4i l = linesP[i];
         line(input, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 2);
     }
+
+    for (size_t i = 0; i < linesIdeal.size(); i++) {
+        Vec4i l = linesIdeal[i];
+        std::cout << "Point1: ";
+        std::cout << l[0] << " " << l[1] << std::endl;
+        std::cout << "Point2: ";
+        std::cout << l[2] << " " << l[3] << std::endl;
+    }
+
+    std::cout << "Imagine urata" << endl << endl;
+    for (size_t i = 0; i < linesP.size(); i++) {
+        Vec4i l = linesP[i];
+        std::cout << "Point1: ";
+        std::cout << l[0] << " " << l[1] << std::endl;
+        std::cout << "Point2: ";
+        std::cout << l[2] << " " << l[3] << std::endl;
+    }
+
+
+    Point2f srcTri[4];
+//    srcTri[0] = Point2f(0.f, 0.f);
+//    srcTri[1] = Point2f(input.cols - 1.f, 0.f);
+//    srcTri[2] = Point2f(0.f, input.rows - 1.f);
+//
+//    Point2f dstTri[3];
+//    dstTri[0] = Point2f(0.f, input.rows * 0.33f);
+//    dstTri[1] = Point2f(input.cols * 0.85f, input.rows * 0.25f);
+//    dstTri[2] = Point2f(input.cols * 0.15f, input.rows * 0.7f);
+
+    srcTri[0] = Point2f(29,35);
+    srcTri[1] = Point2f(29,229);
+    srcTri[2] = Point2f(65,32);
+    srcTri[3] = Point2f(243,218);
+
+    Point2f dstTri[4];
+    dstTri[0] = Point2f(46,230);
+    dstTri[1] = Point2f(228,220);
+    dstTri[2] = Point2f(46,167);
+    dstTri[3] = Point2f(input.cols,input.rows);
+
+//    Mat warp_dst = Mat::zeros(input.rows, input.cols, input.type());
+//    Mat warp_mat = getPerspectiveTransform(srcTri, dstTri);
+//    warpPerspective(input, warp_dst, warp_mat, warp_dst.size());
+
+    Mat warp_dst2 = Mat::zeros(idealPhoto.rows, idealPhoto.cols, input.type());
+    Mat warp_mat2 = getAffineTransform(srcTri, dstTri);
+    warpAffine(idealPhoto, warp_dst2, warp_mat2, warp_dst2.size());
+
+//    imshow("Warp", warp_dst);
+    imshow("Warp affine", warp_dst2);
 
     imshow("Input", input);
     imshow("Output", canny);
